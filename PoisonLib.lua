fdd6cc4d966de95fd7c2dfcbb2fb669ccc8a2623
Ki Night <kinightrd@gmail.com>
2024-12-15 17:01:55 -0300

    Update PoisonLib.lua

--- PoisonLib.lua
+++ PoisonLib.lua
@@ -126,9 +126,9 @@ Title.Position = UDim2.new(0.0339285731, 0, 0.0564263314, 0)
 Title.Size = UDim2.new(0, 200, 0, 23)
 Title.Font = Enum.Font.GothamSemibold
 Title.Text = text
-Title.TextColor3 = Color3.fromRGB(255, 255, 255) -- Define a cor do texto como branco
-Title.TextTransparency = 0 -- Deixa o texto totalmente visível
-Title.TextSize = 12.000
+Title.TextColor3 = Color3.fromRGB(255, 255, 255) -- Cor branca
+Title.TextTransparency = 0 -- Sem transparência
+Title.TextSize = 18 -- Aumenta o tamanho do texto
 Title.TextXAlignment = Enum.TextXAlignment.Left
 
 DragFrame.Name = "DragFrame"
